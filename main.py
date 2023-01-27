609ab81f0e77b7fe4d8d151b643e56b00fdde9c1
1517557055 <112255499+1517557055@users.noreply.github.com>
2023-01-27 20:57:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -71,6 +71,6 @@ wm = WeChatMessage(client)
 luckynum, luckycolor, todate = get_constellation()
 wea, temperature = get_weather()
 data = {"lucknum":{"value":luckynum, "color":get_random_color()},"luckcolor":{"value":luckycolor, "color":get_random_color()},"date":{"value":todate, "color":get_random_color()},"city":{"value":city, "color":get_random_color()},"weather":{"value":wea, "color":get_random_color()},"temperature":{"value":temperature, "color":get_random_color()},"love_days":{"value":get_count(), "color":get_random_color()},"sis_name":{"value":sisname, "color":get_random_color()},"birthday_left":{"value":get_birthday(), "color":get_random_color()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
+
 res = wm.send_template(user_idd, template_id, data)
 print(res)
